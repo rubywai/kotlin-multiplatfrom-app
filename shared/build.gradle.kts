20fff8a1d1f241807c4755f1e494d3f28ae19078
waiphyoaung <rubyland.myanmar.mg@gmail.com>
2023-11-18 17:55:09 +0630

    add rest api with ktor

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     kotlin("multiplatform")
     id("com.android.library")
     id("org.jetbrains.compose")
+    alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.skieTouchlab)
 }
 
 kotlin {
@@ -28,6 +30,15 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.serialization.kotlinx.json)
+                implementation(libs.skie.annotations)
+                implementation(libs.kermit)
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.kamel.image)
+
             }
         }
         val androidMain by getting {
@@ -35,6 +46,7 @@ kotlin {
                 api("androidx.activity:activity-compose:1.7.2")
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.10.1")
+                implementation(libs.ktor.client.android)
             }
         }
         val iosX64Main by getting
@@ -45,10 +57,15 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies{
+                implementation(libs.ktor.client.darwin)
+            }
         }
         val desktopMain by getting {
             dependencies {
                 implementation(compose.desktop.common)
+                implementation(libs.ktor.client.jvm)
+                implementation(libs.ktor.client.logging)
             }
         }
     }
--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,41 +1,104 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
+        val coroutineScopes = rememberCoroutineScope()
+        var requestState: RequestState by remember {
+            mutableStateOf(RequestState.Idle)
+        }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
+                coroutineScopes.launch {
+                    ProductsApi().fetchProducts(limit = 10).collectLatest {
+                        requestState = it
+                    }
+                }
             }) {
-                Text(greetingText)
+                Text("Fetch Data")
             }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    contentDescription = "Compose Multiplatform icon"
-                )
+            when (requestState) {
+                is RequestState.Loading -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                        )
+                    }
+                }
+
+                is RequestState.Success -> {
+                    Card {
+                        LazyColumn(
+                            modifier = Modifier
+                                .background(MaterialTheme.colors.surface)
+                                .padding(horizontal = 20.dp),
+                            verticalArrangement = Arrangement.spacedBy(20.dp)
+                        ) {
+                            items(
+                                count = requestState.getProducts().items.count()
+                            ) {
+                                Column {
+                                    Text(requestState.getProducts().items[it].title)
+                                    Text(requestState.getProducts().items[it].category)
+                                    ProductImage(requestState.getProducts().items[it].image)
+                                }
+                            }
+                        }
+                    }
+                }
+
+                is RequestState.Error -> {
+                    Text("Error")
+                }
+
+                else -> {
+                    Text("Nothing")
+                }
             }
+
         }
     }
+}
+
+@Composable
+fun ProductImage(image: String) {
+    KamelImage(
+        asyncPainterResource(image),
+        "image",
+        Modifier.fillMaxWidth()
+            .height(100.dp)
+
+    )
 }
 
 expect fun getPlatformName(): String
